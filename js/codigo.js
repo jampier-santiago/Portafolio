392680262d7a19c0cdb8254e46656dcba062aa8d
jampier <jampiersantiago@gmail.com>
2021-02-22 16:01:29 -0500

    Se creo la sección de habilidades

---
+++ js/codigo.js
@@ -0,0 +1,34 @@
+/* 
+    --- MOVIMIENTO DEL SLIDER DE HABILIDADES ---
+
+    Parte del codigo encargada de mover los contenedores de habilidades con respecto a la necesidad des usuario
+*/
+const $botonDerecho = document.getElementById('derecha');
+const $botonIzquierdo = document.getElementById('izquierda');
+const $contenedores = document.getElementsByClassName('tarjeta-habilidad');
+let posicion = 0;
+
+$botonDerecho.addEventListener('click', avanzar);
+$botonIzquierdo.addEventListener('click', retroceder);
+
+function avanzar(){
+    posicion += 100;
+    if(posicion == 700){
+        posicion = 0;
+    }
+    for(let i = 0; i < $contenedores.length; i++){
+        $contenedores[i].style.transform = `translateX(-${posicion}%)`;
+    }
+    console.log(posicion);
+}
+
+function retroceder(){
+    posicion -= 100;
+    if(posicion == -100){
+        posicion = 600;
+    }
+    for(let i = 0; $contenedores.length; i++){
+        $contenedores[i].style.transform = `translateX(-${posicion}%)`;
+    }
+    console.log(posicion);
+}
